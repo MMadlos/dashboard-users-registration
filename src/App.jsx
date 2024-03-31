e7accd0bcbd84702ea621b2f1abd8910f9ab6df1
MMadlos <michaelmadlos@gmail.com>
2024-03-31 19:47:58 +0200

    Move reset button from App to UserList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "@/App.css";
 
 import UserList from "@/components/UserList";
-import Button from "@/components/Button";
 
 //TODO -> Fetch instead of mockUserList
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="container flex flex-col gap-8">
       <h1 className="text-3xl">Listado</h1>
-      <Button text="Reset data" />
       <UserList />
     </div>
   );
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -12,45 +12,51 @@ export default function UserList() {
   }
 
   return (
-    <table className="w-full">
-      <thead>
-        <tr className="border-b border-stone-600">
-          <th className="py-2">Foto</th>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Email</th>
-          <th>País</th>
-          <th>Fecha registro</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {userList.map((userData) => {
-          const { picture, name, email, location, registered, login } =
-            userData;
-          const { uuid } = login;
+    <>
+      <Button
+        text="Reset data"
+        onClick={() => setUserList(mockUserList.results)}
+      />
+      <table className="w-full">
+        <thead>
+          <tr className="border-b border-stone-600">
+            <th className="py-2">Foto</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>País</th>
+            <th>Fecha registro</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.map((userData) => {
+            const { picture, name, email, location, registered, login } =
+              userData;
+            const { uuid } = login;
 
-          return (
-            <tr key={uuid}>
-              <td className="py-2">
-                <img
-                  src={picture.thumbnail}
-                  alt=""
-                  className="mx-auto rounded-full "
-                />
-              </td>
-              <td>{name.first}</td>
-              <td>{name.last}</td>
-              <td>{email}</td>
-              <td>{location.country}</td>
-              <td>{registered.date}</td>
-              <td>
-                <Button text="Delete" onClick={() => removeUser(uuid)} />
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr key={uuid}>
+                <td className="py-2">
+                  <img
+                    src={picture.thumbnail}
+                    alt=""
+                    className="mx-auto rounded-full "
+                  />
+                </td>
+                <td>{name.first}</td>
+                <td>{name.last}</td>
+                <td>{email}</td>
+                <td>{location.country}</td>
+                <td>{registered.date}</td>
+                <td>
+                  <Button text="Delete" onClick={() => removeUser(uuid)} />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
