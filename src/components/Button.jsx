f4526a8a5b2663f1e7744c2b9f25473297cb8ac4
MMadlos <michaelmadlos@gmail.com>
2024-03-31 21:01:58 +0200

    Add className to fn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({ onClick, text }) {
+export default function Button({ onClick, text, className }) {
   return (
     <button
-      className="bg-stone-700 rounded-sm py-1 px-8 hover:opacity-80 w-fit"
+      className={`bg-stone-700 rounded-sm py-1 px-8 hover:opacity-80 w-fit ${className}`}
       onClick={onClick}
     >
       {text}
