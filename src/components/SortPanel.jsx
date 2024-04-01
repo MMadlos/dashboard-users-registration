fafdc6bf141e80e394bbe0e9f6049422adf620fe
MMadlos <michaelmadlos@gmail.com>
2024-04-01 18:10:20 +0200

    Add button disabled if no parameter is selected

--- src/components/SortPanel.jsx
+++ src/components/SortPanel.jsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 
 import Button from "./Button";
 
-export default function SortPanel() {
+export default function SortPanel({ onSubmit }) {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
     <div className="flex flex-col justify-start gap-2 ">
       <Button text="Sort" onClick={() => setIsVisible(!isVisible)} />
-      {isVisible && <SortOptions />}
+      {isVisible && <SortOptions onSubmit={onSubmit} />}
     </div>
   );
 }
 
-function SortOptions() {
+function SortOptions({ onSubmit }) {
+  const [sortSelected, setSortSelected] = useState("default");
+
   const options = {
     default: "Selecciona columna",
     firstName: "Nombre",
@@ -22,34 +24,23 @@ function SortOptions() {
     date: "Fecha registro",
   };
 
-  function handleSubmitSort(e) {
-    e.preventDefault();
-
-    const form = e.target;
-    const formData = new FormData(form);
-    const formJson = Object.fromEntries(formData.entries());
-
-    console.log(formJson);
-
-    const { column: option } = formJson;
-
-    if (option === "default")
-      console.log("No se ha seleccionado ningún parámetro");
-
-    if (option !== "default") {
-      console.log(`Se ha ordenado la columna "${options[option]}"`);
-    }
+  function handleOnChange(e) {
+    const { value } = e.target;
+    setSortSelected(value);
   }
 
+  const isDefault = sortSelected === "default";
+
   return (
-    <form onSubmit={handleSubmitSort}>
+    <form onSubmit={onSubmit}>
       <div className=" bg-stone-800 p-4 rounded-sm flex flex-row gap-4">
         <div className="flex gap-4 items-center">
           <label htmlFor="column">Parámetro:</label>
           <select
             name="column"
             id="column-param"
-            className="bg-stone-700 rounded-sm px-4 py-2 hover:cursor-pointer"
+            className="bg-stone-700 rounded-sm px-4 py-2 hover:cursor-pointer hover:opacity-80"
+            onChange={handleOnChange}
           >
             {Object.entries(options).map(([value, text], index) => {
               return (
@@ -66,13 +57,16 @@ function SortOptions() {
           <select
             name="type"
             id="type-sort"
-            className="bg-stone-700 rounded-sm px-4 py-2 hover:cursor-pointer"
+            className="bg-stone-700 rounded-sm px-4 py-2 hover:cursor-pointer hover:opacity-80"
           >
             <option value="asc">Ascendente</option>
             <option value="des">Descendente</option>
           </select>
         </div>
-        <button className="w-full px-6 py-2 bg-emerald-700 rounded">
+        <button
+          className="w-full px-6 py-2 bg-emerald-700 rounded hover:opacity-80 disabled:opacity-20"
+          disabled={isDefault}
+        >
           Aplicar
         </button>
       </div>
