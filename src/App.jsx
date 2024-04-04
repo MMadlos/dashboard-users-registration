d69b5a2fd5eb0981bc9f8a93bda51a40eb026c49
MMadlos <michaelmadlos@gmail.com>
2024-04-04 13:56:55 +0200

    Add files for Chart JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "@/App.css";
 
 import UserList from "@/components/UserList";
+import Chart from "./components/Chart";
 
 //TODO -> Fetch instead of mockUserList
 
 function App() {
   return (
     <div className="container flex flex-col gap-8">
-      <h1 className="text-3xl">Listado</h1>
+      <h2 className="text-3xl">Gráfico</h2>
+      <Chart />
+      <h2 className="text-3xl">Listado</h2>
       <UserList />
     </div>
   );
---
+++ src/components/Chart.jsx
@@ -0,0 +1,7 @@
+export default function Chart() {
+  return (
+    <div className="w-full min-h-5 bg-stone-800">
+      <canvas className="myChart"></canvas>
+    </div>
+  );
+}
